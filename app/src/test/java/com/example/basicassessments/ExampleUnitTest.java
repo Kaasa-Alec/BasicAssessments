6fec21e34905dc12c02c6a3ce5fcd1d8b329b7db
Kaasa-Alec <kaa15001@byui.edu>
2018-11-07 20:16:06 -0800

    unit test practice

--- app/src/test/java/com/example/basicassessments/ExampleUnitTest.java
+++ app/src/test/java/com/example/basicassessments/ExampleUnitTest.java
@@ -10,8 +10,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    String input = "Bugs Bunny";
+
+    int scoreInput = 6;
+
+    String question = "Can I have something to eat?";
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+    public void addition_isCorrect() { assertEquals(4, 2 + 2); }
+
+    @Test
+    public void addStudentTest() { assertEquals("Bugs Bunny", input); }
+
+    @Test
+    public void checkScoreTest() { assertTrue(scoreInput >= 0 && scoreInput < 10); }
+
+    @Test
+    public void checkQuestionIsString() { assertTrue(question instanceof String); }
+
 }
