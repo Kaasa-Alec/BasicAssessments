549f616cf252d60b5fa3b363b7cbe0f7ecfc55df
Kaasa-Alec <kaa15001@byui.edu>
2018-11-09 21:32:09 -0800

    Some initial view stuff

---
+++ app/src/main/java/com/example/basicassessments/currentStudentsActivity.java
@@ -0,0 +1,13 @@
+package com.example.basicassessments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class currentStudentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_students);
+    }
+}
---
+++ app/src/main/java/com/example/basicassessments/newStudentActivity.java
@@ -0,0 +1,23 @@
+package com.example.basicassessments;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class newStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_student);
+    }
+
+    /*public void cancelButton(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
+    }*/
+}
