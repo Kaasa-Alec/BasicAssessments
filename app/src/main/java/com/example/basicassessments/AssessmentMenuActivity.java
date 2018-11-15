97bf80438d105dac0f2ce2789ba23a85cec7e60c
Mona Lisa <had17007@byui.edu>
2018-11-14 20:41:06 -0700

    The second push for fonts and views.

---
+++ app/src/main/java/com/example/basicassessments/AssessmentMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.basicassessments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssessmentMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_menu);
+    }
+}
