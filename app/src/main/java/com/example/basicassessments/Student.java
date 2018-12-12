2afc0bd38071066153e943c45a7e327efc951540
Mona Lisa <had17007@byui.edu>
2018-12-11 22:00:25 -0700

    A working config before I jump off the deep end.

--- app/src/main/java/com/example/basicassessments/Student.java
+++ app/src/main/java/com/example/basicassessments/Student.java
@@ -27,33 +27,33 @@ public class Student {
     @ColumnInfo(name = "student_name")
     private String name;
 
-    /**
-     * Holds all quarters and their assessments.
-     */
-    @ColumnInfo(name = "student_quarters")
-    private ArrayList<Quarter> quarters = new ArrayList<>();
-
-    /**
-     * Non-Default constructor that takes a lunch id and student name and populates all their
-     * quarters.
-     *
-     * @param id Lunch number of student.
-     * @param name Student name.
-     */
-    public Student(int id, String name) {
-        this.id = id;
-        this.name = name;
-
-        Quarter q0 = new Quarter();
-        Quarter q1 = new Quarter();
-        Quarter q2 = new Quarter();
-        Quarter q3 = new Quarter();
-
-        quarters.add(q0);
-        quarters.add(q1);
-        quarters.add(q2);
-        quarters.add(q3);
-    }
+//    /**
+//     * Holds all quarters and their assessments.
+//     */
+//    @ColumnInfo(name = "student_quarters")
+//    private ArrayList<Quarter> quarters = new ArrayList<>();
+//
+//    /**
+//     * Non-Default constructor that takes a lunch id and student name and populates all their
+//     * quarters.
+//     *
+//     * @param id Lunch number of student.
+//     * @param name Student name.
+//     */
+//    public Student(int id, String name) {
+//        this.id = id;
+//        this.name = name;
+//
+//        Quarter q0 = new Quarter();
+//        Quarter q1 = new Quarter();
+//        Quarter q2 = new Quarter();
+//        Quarter q3 = new Quarter();
+//
+//        quarters.add(q0);
+//        quarters.add(q1);
+//        quarters.add(q2);
+//        quarters.add(q3);
+//    }
 
 
     // ID Get/Set
